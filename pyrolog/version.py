4c410b09e946da8b839287d08049ac798faf1c64
ftdot <ftdoot@gmail.com>
2023-07-12 23:54:34 +0300

    Extend version

--- pyrolog/version.py
+++ pyrolog/version.py
@@ -5,5 +5,5 @@ __all__ = ['__version__', '__version_info__']
 
 VersionInfo = namedtuple('VersionInfo', ['major', 'minor', 'patch', 'release_level', 'commit'])
 
-__version__ = (2, 0, 0)
-__version_info__ = VersionInfo(*__version__, 'beta', 'c0783f3')
+__version__ = (2, 1, 0)
+__version_info__ = VersionInfo(*__version__, 'beta', '3af325d')
