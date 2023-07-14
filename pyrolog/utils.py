463691c0660b89067155c38dff82aa076932bf75
ftdot <ftdoot@gmail.com>
2023-07-14 14:51:05 +0300

    Add get_filename_timestamp() function for log files

--- pyrolog/utils.py
+++ pyrolog/utils.py
@@ -1,14 +1,20 @@
 
 import inspect
 
+from datetime import datetime
+
 from .logging_context import LoggingContext
-from .defaults import DEFAULT_LOGGING_CONTEXT
+from .defaults import DEFAULT_LOGGING_CONTEXT, MAXIMUM_TIME_FORMAT_STRING_FILENAME_SAFE
 from .formatters import PlainFormatter, defined_formatters
 
 from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from .logger import Logger
+
+
+__all__ = ['make_logger_binding', 'make_new_log_level', 'update_logger_name_offset',
+           'update_group_name_offset', 'get_filename_timestamp']
 
 
 def make_logger_binding(level: int):
@@ -44,3 +50,6 @@ def update_group_name_offset(logging_context: LoggingContext = DEFAULT_LOGGING_C
         if hasattr(f, 'offsets') and f.offsets and f.logging_context is logging_context:
             f.static_variables['group_name_offset'] = logging_context.get_group_name_offset()
 
+
+def get_filename_timestamp(format_string=MAXIMUM_TIME_FORMAT_STRING_FILENAME_SAFE + '.log'):
+    return PlainFormatter(time_format_string=format_string).format_time(datetime.now())
